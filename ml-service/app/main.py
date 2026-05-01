0e6dbe33d175572be5002ccf7c6ba093282de23f
Mei-IwI <c2005vn@gmail.com>
2026-05-01 21:13:11 +0700

    add model predict and model_loader

--- ml-service/app/main.py
+++ ml-service/app/main.py
@@ -1,17 +1,23 @@
 from fastapi import FastAPI
 from app.schemas.request import PredictionRequest
 from app.schemas.response import PredictionResponse
+from app.services.model_loader import load_model_bundle
+from app.services.predictor import predict_score
 
 app = FastAPI(title="Predict The Score API")
 
+@app.on_event("startup")
+def startup_load_model():
+    load_model_bundle()
+
 @app.get("/")
 def root():
-    return {"message": "ML service is running"}
+    return {"message": "Machine Learning is running"}
 
 @app.post("/predict", response_model=PredictionResponse)
 def predict(payload: PredictionRequest):
-    predicted_score = 12.5
+    predicted_score = predict_score(payload)
     return PredictionResponse(
         predicted_score=predicted_score,
-        model_name="baseline-demo"
+        model_name="linear-regression-web-minimal"
     )
